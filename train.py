6ff8cf2165aed4672b6f1b19328d5b24165102dc
kefth <kefth@users.noreply.github.com>
2018-06-21 14:44:51 +0200

    Migrating to 0.4

--- train.py
+++ train.py
@@ -1,10 +1,6 @@
 import torch
-import torchvision.transforms as transforms
-from torch.autograd import Variable
+from torchvision import datasets, models, transforms
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from fashionmnist import FashionMNIST
-from torchvision import datasets
 import model
 import utils
 import time
@@ -14,20 +10,19 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default='FashionSimpleNet', help="model")
 parser.add_argument("--patience", type=int, default=5, help="early stopping patience")
-parser.add_argument("--batch_size", type=int, default=64, help="batch size")
+parser.add_argument("--batch_size", type=int, default=256, help="batch size")
 parser.add_argument("--nepochs", type=int, default=200, help="max epochs")
-parser.add_argument("--nocuda", action='store_true', help="no cuda used")
 parser.add_argument("--nworkers", type=int, default=4, help="number of workers")
 parser.add_argument("--seed", type=int, default=1, help="random seed")
-parser.add_argument("--data", type=str, default='fashion', help="mnist or fashion")
 args = parser.parse_args()
 
-cuda = not args.nocuda and torch.cuda.is_available() # use cuda
-print('Training on cuda: {}'.format(cuda))
+# Set up the device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('Training on {}'.format(device))
 
 # Set seeds. If using numpy this must be seeded too.
 torch.manual_seed(args.seed)
-if cuda:
+if device== 'cuda:0':
     torch.cuda.manual_seed(args.seed)
 
 # Setup folders for saved models and logs
@@ -36,7 +31,7 @@ if not os.path.exists('saved-models/'):
 if not os.path.exists('logs/'):
     os.mkdir('logs/')
 
-# Setup tensorboard folders. Each run must have it's own folder. Creates
+# Setup folders. Each run must have it's own folder. Creates
 # a logs folder for each model and each run.
 out_dir = 'logs/{}'.format(args.model)
 if not os.path.exists(out_dir):
@@ -44,122 +39,101 @@ if not os.path.exists(out_dir):
 run = 0
 current_dir = '{}/run-{}'.format(out_dir, run)
 while os.path.exists(current_dir):
-	run += 1
-	current_dir = '{}/run-{}'.format(out_dir, run)
+    run += 1
+    current_dir = '{}/run-{}'.format(out_dir, run)
 os.mkdir(current_dir)
 logfile = open('{}/log.txt'.format(current_dir), 'w')
 print(args, file=logfile)
 
-# Tensorboard viz. tensorboard --logdir {yourlogdir}. Requires tensorflow.
-from tensorboard_logger import configure, log_value
-configure(current_dir, flush_secs=5)
+
 
 # Define transforms.
-# normalize = transforms.Normalize((0.1307,), (0.3081,)
 train_transforms = transforms.Compose([
-                        # transforms.RandomHorizontalFlip(),
-                        # utils.RandomRotation(),
-                        # utils.RandomTranslation(),
-                        # utils.RandomVerticalFlip(),
-                        transforms.ToTensor()
-                        # transforms.Normalize((0.1307,), (0.3081,))
-                        ]
-                        )
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
 val_transforms = transforms.Compose([
-                        transforms.ToTensor()
-                        # transforms.Normalize((0.1307,), (0.3081,))
-                        ])
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
 
 # Create dataloaders. Use pin memory if cuda.
-kwargs = {'pin_memory': True} if cuda else {}
-if(args.data == 'mnist'):
-    trainset = datasets.MNIST('data-mnist', train=True, download=True, transform=train_transforms)
-    train_loader = DataLoader(trainset, batch_size=args.batch_size,
-                            shuffle=True, num_workers=args.nworkers, **kwargs)
-    valset = datasets.MNIST('data-mnist', train=False, transform=val_transforms)
-    val_loader = DataLoader(valset, batch_size=args.batch_size,
-                            shuffle=False, num_workers=args.nworkers, **kwargs)
-else:
-    trainset = FashionMNIST('data', train=True, download=True, transform=train_transforms)
-    train_loader = DataLoader(trainset, batch_size=args.batch_size,
-                            shuffle=True, num_workers=args.nworkers, **kwargs)
-    valset = FashionMNIST('data', train=False, transform=val_transforms)
-    val_loader = DataLoader(valset, batch_size=args.batch_size,
-                            shuffle=False, num_workers=args.nworkers, **kwargs)
+kwargs = {'pin_memory': True} if device=='cuda:0' else {}
 
-def train(net, loader, criterion, optimizer):
-    net.train()
+trainset = datasets.FashionMNIST('./data', train=True, download=True, transform=train_transforms)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                          shuffle=True, num_workers=args.nworkers, **kwargs)
+valset = datasets.FashionMNIST('data', train=False, transform=val_transforms)
+val_loader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size,
+                        shuffle=False, num_workers=args.nworkers, **kwargs)
+
+
+def run_model(net, loader, criterion, optimizer, train = True):
     running_loss = 0
     running_accuracy = 0
 
-    for i, (X,y) in enumerate(loader):
-        if cuda:
-            X, y = X.cuda(), y.cuda()
-        X, y = Variable(X), Variable(y)
+    # Set mode
+    if train:
+        net.train()
+    else:
+        net.eval()
 
-        output = net(X)
-        loss = criterion(output, y)
+
+    for i, (X, y) in enumerate(loader):
+        # Pass to gpu or cpu
+        X, y = X.to(device), y.to(device)
+
+        # Zero the gradient
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.data[0]
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        running_accuracy += pred.eq(y.data.view_as(pred)).cpu().sum()
-    return running_loss/len(loader), running_accuracy/len(loader.dataset)
 
-def validate(net, loader, criterion):
-    net.eval()
-    running_loss = 0
-    running_accuracy = 0
-    for i, (X,y) in enumerate(loader):
-        if cuda:
-            X, y = X.cuda(), y.cuda()
-        X, y = Variable(X, volatile=True), Variable(y)
-        output = net(X)
-        loss = criterion(output, y)
-        running_loss += loss.data[0]
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        running_accuracy += pred.eq(y.data.view_as(pred)).cpu().sum()
-    return running_loss/len(loader), running_accuracy/len(loader.dataset)
+        with torch.set_grad_enabled(train):
+            output = net(X)
+            _, pred = torch.max(output, 1)
+            loss = criterion(output, y)
+
+        # If on train backpropagate
+        if train:
+            loss.backward()
+            optimizer.step()
+
+        # Calculate stats
+        running_loss += loss.item()
+        running_accuracy += torch.sum(pred == y.detach())
+    return running_loss / len(loader), running_accuracy / len(loader)
+
 
 
 if __name__ == '__main__':
+
+    # Init network, criterion and early stopping
     net = model.__dict__[args.model]()
     print(net)
     criterion = torch.nn.CrossEntropyLoss()
-
-    if cuda:
-        net, criterion = net.cuda(), criterion.cuda()
-    # early stopping parameters
     patience = args.patience
     best_loss = 1e4
 
     # Print model to logfile
-    print(net, file=logfile)
+    # print(net, file=logfile)
 
-    # Change optimizer for finetuning
+    # Define optimizer
     optimizer = optim.Adam(net.parameters())
 
     for e in range(args.nepochs):
         start = time.time()
-        train_loss, train_acc = train(net, train_loader,
-            criterion, optimizer)
-        val_loss, val_acc = validate(net, val_loader, criterion)
+        train_loss, train_acc = run_model(net, train_loader,
+                                      criterion, optimizer)
+        val_loss, val_acc = run_model(net, train_loader,
+                                      criterion, optimizer, False)
         end = time.time()
 
         # print stats
-        stats ="""Epoch: {}\t train loss: {:.3f}, train acc: {:.3f}\t
+        stats = """Epoch: {}\t train loss: {:.3f}, train acc: {:.3f}\t
                 val loss: {:.3f}, val acc: {:.3f}\t
-                time: {:.1f}s""".format( e, train_loss, train_acc, val_loss,
-                val_acc, end-start)
+                time: {:.1f}s""".format(e, train_loss, train_acc, val_loss,
+                                        val_acc, end - start)
         print(stats)
-        print(stats, file=logfile)
-        log_value('train_loss', train_loss, e)
-        log_value('val_loss', val_loss, e)
-        log_value('train_acc', train_acc, e)
-        log_value('val_acc', val_acc, e)
 
-        #early stopping and save best model
+        # early stopping and save best model
         if val_loss < best_loss:
             best_loss = val_loss
             patience = args.patience
